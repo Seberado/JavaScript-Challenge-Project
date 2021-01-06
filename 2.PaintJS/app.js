aae96c4f70d57ab61788d478b4ad838c84b3e4c1
Andrey Sinaf <68128472+Seberado@users.noreply.github.com>
2021-01-07 02:57:24 +0300

    fix: app.js  onMouse states

--- 2.PaintJS/app.js
+++ 2.PaintJS/app.js
@@ -1,10 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
 
 
-function onMouseMove(event){
-console.log(event);
+let painting = false;
+
+function stopPainting(){
+    painting = false;
 }
 
+
+function onMouseMove(event){
+
+const x = event.offsetX;
+const y = event.offsetY;
+
+}
+
+function onMouseDown(event){
+    painting = true;
+
+}
+
+function onMouseUp(event){
+    // painting = false;
+    stopPainting();
+    
+}
+
+function onMouseLeave(event){
+    painting = false;
+}
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
 }
